5ae74f4af17380389034f362b7325a85868d1eb8
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-06-28 21:14:21 +0800

    feat: update response types

--- src/common/structs/response.rs
+++ src/common/structs/response.rs
@@ -1,22 +1,31 @@
+use literalize::literal;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 /// JSON response error.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct JsonResponseErrorStruct<ErrorCode = String> {
+pub struct JsonResponseErrorStruct<
+    Code = String,
+    Path = String,
+    Message = String,
+> {
     /// Code representing the error.
-    pub code: ErrorCode,
+    pub code: Code,
     /// Indicates where the error occurred.
-    pub path: Vec<String>,
+    pub path: Vec<Path>,
     /// Detail of the error.
-    pub message: Option<String>,
+    pub message: Option<Message>,
 }
+
+#[derive(Clone)]
+#[literal(true)]
+pub struct LiteralTrue;
 
 /// Success JSON response.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct JsonSuccessResponse<Data = ()> {
     /// Indicates whether the response is successful or not.
-    pub success: bool,
+    pub success: LiteralTrue,
     /// Requested information for the response when `success` is `true`.
     pub data: Data,
     /// A list of errors for the response when `success` is `false`.
@@ -24,13 +33,17 @@ pub struct JsonSuccessResponse<Data = ()> {
     pub errors: Vec<JsonResponseErrorStruct>,
 }
 
+#[derive(Clone)]
+#[literal(false)]
+pub struct LiteralFalse;
+
 /// Failure JSON response.
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct JsonFailureResponse<Code = String> {
+pub struct JsonFailureResponse<Error = JsonResponseErrorStruct> {
     /// Indicates whether the response is successful or not.
-    pub success: bool,
+    pub success: LiteralFalse,
     /// Requested information for the response when `success` is `true`.
     pub data: Option<()>,
     /// A list of errors for the response when `success` is `false`.
-    pub errors: Vec<JsonResponseErrorStruct<Code>>,
+    pub errors: Vec<Error>,
 }
